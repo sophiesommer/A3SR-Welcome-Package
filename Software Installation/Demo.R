181e849e16e4421208cadc97d4b5a81453fa5ff6
Sophie Sommer <sjs908@nyu.edu>
2019-06-19 16:28:11 -0400

    creating a demo script for R video

---
+++ Software Installation/Demo.R
@@ -0,0 +1,13 @@
+#save welcome message
+welcome <- "Welcome to A3SR!"
+#print welcome message  
+print(welcome)
+
+#save my favorite numbers
+My_Favorite_Numbers <- c(3,7,9,13,17,27,51,327,513)
+#inspect the mean of my favorite numbers
+mean(My_Favorite_Numbers)
+#how many favorite numbers do I have?
+length(My_Favorite_Numbers)
+
+
